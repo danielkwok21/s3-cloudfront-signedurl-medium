63d07a59b18f623f0b45140148ffa8839243d055
daniel kwok <danielzhengxian21@gmail.com>
2022-02-09 22:55:55 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# How to setup cloudfront signed url + s3
+
+To be used in conjunction with this medium article
+
+Referenced from https://medium.com/@apalshah/node-js-serve-private-content-using-aws-cloudfront-992e846259ae
+
+## Getting started
+
+1. Run `npm install` to install dependencies
+2. Run `node app.js` to get signed url. Should look something like https://dnl0sjq1a303m.cloudfront.net/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?Expires=1644418040349&Key-Pair-Id=APKAIB2BNQPS2D34NPJA&Signature=CPn6OgmuzHHVyzKOSLAcyL9He1lzEL2HKdlTRm4CoR2khLMjlzH8qAzyyI0pLmFfxajjZGU-9WZU3CrjvPnKURm29sojcog1v5Nc0N4N4ffB2e3aGV1d34b8BIHn7GzD7biFp0RgvR6TkbpTzZSVBMfdNAaoTzsWhfLsX367gXCuA~c70KTGZHj2zD~N99BJfe9AWDWCXqRrJB3FQuJthKPOne9A-ImT4Hvi3jfZark1xH2lZT1WlCbT3E~MNmJkhL05-R2ud5UlAMseuOgchA-0u5Vgen-jNLjDUQQ5BmptgVtMGf988qVf8IbxMt5kmFO2DsTP3Oqol-z-IyCHiA__
+3. Click on link and view in browser.
+
+<img src="https://cdn-images-1.medium.com/max/558/1*btjg3g_98YB5pPvxx1WYXA.png" alt="drawing" width="400"/>
+
+## FYI
+1. Remember to create an invalidation after each changes in cloudfront distribution configuration else updated behavior might be delayed.
+
---
+++ app.js
@@ -0,0 +1,50 @@
+const AWS = require('aws-sdk')
+const fs = require('fs')
+
+/**
+ * From AWS console root user > security credentials
+ * Refer to this screenshot https://cdn-images-1.medium.com/max/716/1*lj0SGfHRLt8pvoe1OLla6Q.png
+ */
+const accessKeyID = 'SOME_ACCESS_KEY_ID'
+
+/**
+ * From local
+ * The contents of the private key file
+ * Refer to this screenshot https://cdn-images-1.medium.com/max/716/1*5VBnhzFnZNRZ0jp3eY9uFA.png
+ */
+const privateKeyFilePath = '/local/path/to/your/private/pemkey/file'
+const privateKeyContents = fs.readFileSync(privateKeyFilePath, 'utf8')
+
+
+/**
+ * From AWS Console > Cloudfront > Distributions
+ * Refer to this screenshot https://cdn-images-1.medium.com/max/716/1*UZI0mq0LJd5Hn2t5psOrwA.png
+ */
+const cfDomainName = `https://yourcloudfront.cloudfront.net`
+
+/**
+ * From AWS Console > (path of our desired content)
+ * Refer to this screenshot https://cdn-images-1.medium.com/max/558/1*MzwwxxEU5FOx2aBI1w7w0A.png
+ */
+const s3ContentPath = `somefolder/somefile.jpg`
+
+const cfFullPath = `${cfDomainName}/${s3ContentPath}`
+let signer = new AWS.CloudFront.Signer(accessKeyID, privateKeyContents)
+
+const option = {
+    url: cfFullpath,
+    expires: Math.floor((new Date()).getTime()) + (60 * 60 * 1), // 1 hour from now
+}
+
+
+return new Promise((res, rej) => {
+    signer.getSignedUrl(option, (err, url) => {
+        if (err) {
+            console.error(err)
+            rej(err)
+        } else {
+            console.log(url);
+            res(url)
+        }
+    })
+})
